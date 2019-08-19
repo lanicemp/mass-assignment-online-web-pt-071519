637c21ad338c407c123714e5d312ff56e50c4aeb
LaNice Powell <lanicemp@aol.com>
2019-08-19 02:42:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color
  
  def initialize(attributes)
-   {}
+  attributes.each {|key, value| self.send(("#{key}="), value)} 
  end 
   #your code here
 end
